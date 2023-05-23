7d82c91bb014111f964f9167abe0ed3a49e25155
damianolagana <damianolagana@gmail.com>
2023-05-23 10:46:54 +0200

    Header structure

--- my-vue-app/src/components/AppHeaderNav.vue
+++ my-vue-app/src/components/AppHeaderNav.vue
@@ -9,7 +9,20 @@
                 <a class="navbar-brand" href="#">
                     <img src="../assets/img/dc-logo.png" alt="Bootstrap" width="30" height="24">
                 </a>
+                <div class="d-flex justify-content-sm-between">
+                    <div>Characters</div>
+                    <div>Comics</div>
+                    <div>Movies</div>
+                    <div>TV</div>
+                    <div>Games</div>
+                    <div>Collectibles</div>
+                    <div>Videos</div>
+                    <div>Fans</div>
+                    <div>News</div>
+                    <div>Shop</div>
+                </div>
             </div>
+
         </nav>
     </header>
 </template>
@@ -18,5 +31,9 @@
 a>img {
     width: 60px;
     height: 60px;
+}
+
+.container>div {
+    width: 60%;
 }
 </style>
