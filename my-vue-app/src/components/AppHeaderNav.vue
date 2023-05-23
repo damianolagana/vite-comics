2b442f02bd7736478554da1a710c07639a9d52b3
damianolagana <damianolagana@gmail.com>
2023-05-23 10:38:04 +0200

    start structure layout

---
+++ my-vue-app/src/components/AppHeaderNav.vue
@@ -0,0 +1,22 @@
+<script>
+
+</script>
+
+<template>
+    <header>
+        <nav class="navbar bg-light">
+            <div class="container">
+                <a class="navbar-brand" href="#">
+                    <img src="../assets/img/dc-logo.png" alt="Bootstrap" width="30" height="24">
+                </a>
+            </div>
+        </nav>
+    </header>
+</template>
+
+<style scoped>
+a>img {
+    width: 60px;
+    height: 60px;
+}
+</style>
